6e3a7b8da96e86e75296427176d3dc3ecaca8e3d
deninjo <imwendwadennis@gmail.com>
2023-05-01 22:54:39 +0300

    Rectified from 80% to 100% zoom

--- script.js
+++ script.js
@@ -8,3 +8,43 @@ registerLink.addEventListener('click', ()=> {wrapper.classList.add('active')})
 loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active')})
 btnPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup')})
 iconClose.addEventListener('click', ()=> {wrapper.classList.remove('active-popup')})
+
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const submitted = urlParams.get('submitted');
+  if (submitted === 'true') {
+    document.getElementById('message').textContent = 'Added to wishlist';
+  }
+
+
+/*property listing
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+*/
